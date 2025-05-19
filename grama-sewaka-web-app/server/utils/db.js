3526c2fe499c64a98074ec1935cf835ae16bd924
Chamindu-08 <155412077+Chamindu-08@users.noreply.github.com>
2025-05-19 08:21:50 +0530

    Created a MySQL connection pool using mysql2

---
+++ grama-sewaka-web-app/server/utils/db.js
@@ -0,0 +1,38 @@
+import mysql2 from "mysql2/promise";
+import dotenv from "dotenv";
+import logger from "./logger.js";
+
+dotenv.config();
+
+export const pool = mysql2.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+
+export const connectDB = async () => {
+  try {
+    await pool.getConnection();
+    logger.info("Connected to the database");
+  } catch (error) {
+    logger.error("Error connecting to the database:", error);
+    throw error;
+  }
+};
+
+export const closeDB = async () => {
+  try {
+    await pool.end();
+    logger.info("Database connection pool closed");
+  } catch (error) {
+    logger.error("Error closing the database connection pool:", error);
+    throw error;
+  }
+};
+
+export default pool;
