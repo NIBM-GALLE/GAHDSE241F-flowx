a307a9bb3078c218d4798a17d4ea6e84949046f2
Chamindu-08 <155412077+Chamindu-08@users.noreply.github.com>
2025-05-19 07:28:10 +0530

    initialize the server

--- grama-sewaka-web-app/server/src/main/java/com/flowx/server/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flowx.server.repositories;
-
-import com.flowx.server.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-    User findByUserEmail(String email);
-}
